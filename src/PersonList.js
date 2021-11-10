ac5dd1944e81e4886307a2c8fbe3ac7bec9788a0
robert-otieno <robertotieno.or@gmail.com>
2021-11-10 12:46:38 +0300

    How To Use Axios with React

--- src/PersonList.js
+++ src/PersonList.js
@@ -1,25 +1,127 @@
 import React from 'react';
-
+import API from './api';
 import axios from 'axios';
 
 export default class PersonList extends React.Component {
-  state = {
-    persons: []
-  }
+// Making a GET Request
+// ----------------------------------------------
 
-  componentDidMount() {
-    axios.get(`https://jsonplaceholder.typicode.com/users`)
-      .then(res => {
-        const persons = res.data;
-        this.setState({ persons });
-      })
-  }
+//   state = {
+//     persons: []
+//   }
+
+//   componentDidMount() {
+//     axios.get(`https://jsonplaceholder.typicode.com/users`)
+//       .then(res => {
+//         const persons = res.data;
+//         this.setState({ persons });
+//       })
+//   }
+
+//   render() {
+//     return (
+//       <ul>
+//         { this.state.persons.map(person => <li>{person.name}</li>)}
+//       </ul>
+//     )
+//   }
+
+// Making a POST Request
+// ----------------------------------------------
+
+	// state = {
+	// 	name: '',
+	// }
+
+	// handleChange = event => {
+  //   this.setState({ name: event.target.value });
+  // }
+
+  // handleSubmit = event => {
+  //   event.preventDefault();
+
+  //   const user = {
+  //     name: this.state.name
+  //   };
+
+  //   axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
+  //     .then(res => {
+  //       console.log(res);
+  //       console.log(res.data);
+  //     })
+  // }
+
+  // render() {
+  //   return (
+  //     <div>
+  //       <form onSubmit={this.handleSubmit}>
+  //         <label>
+  //           Person Name:
+  //           <input type="text" name="name" onChange={this.handleChange} />
+  //         </label>
+  //         <button type="submit">Add</button>
+  //       </form>
+  //     </div>
+  //   )
+  // }
+
+// Making a DELETE Request
+// ----------------------------------------------
+
+	// state = {
+  //   id: '',
+  // }
+
+  // handleChange = event => {
+  //   this.setState({ id: event.target.value });
+  // }
+
+  // handleSubmit = event => {
+  //   event.preventDefault();
+
+  //   axios.delete(`https://jsonplaceholder.typicode.com/users/${this.state.id}`)
+  //     .then(res => {
+  //       console.log(res);
+  //       console.log(res.data);
+  //     })
+  // }
+
+	// Using a Base Instance in Axios
+	// ----------------------------------------------
+
+	// handleSubmit = event => {
+  //   event.preventDefault();
+
+  //   API.delete(`users/${this.state.id}`)
+  //     .then(res => {
+  //       console.log(res);
+  //       console.log(res.data);
+  //     })
+	// }
+
+	// Using async and await
+	// ----------------------------------------------
+
+	handleSubmit = async event => {
+		event.preventDefault();
+	
+		const response = await API.delete(`users/${this.state.id}`);	
+		console.log(response);
+		console.log(response.data);
+	};
 
   render() {
     return (
-      <ul>
-        { this.state.persons.map(person => <li>{person.name}</li>)}
-      </ul>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Person ID:
+            <input type="text" name="id" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Delete</button>
+        </form>
+      </div>
     )
   }
+
 }
